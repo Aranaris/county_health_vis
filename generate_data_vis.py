eb61648657f5c69a6a90f0a4a0821742659599b5
vinceye <vinceye.cm@gmail.com>
2024-01-19 22:24:37 -0800

    added county name labels to hover on map

--- generate_data_vis.py
+++ generate_data_vis.py
@@ -15,15 +15,16 @@ df = pd.read_csv('data/chd_stroke_data.csv',
 
 df['fips'] = df.apply(lambda x: locationid_to_fips(x['LocationID']), axis=1)
 
-new_df = df[df['Stratification1'] == 'Ages 35-64 years'][df['Topic']=='Stroke'][df['Data_Value_Unit']=='per 100,000'][df['Year']=='2018'][['fips','Data_Value']]
+new_df = df[df['Stratification1'] == 'Ages 35-64 years'][df['Topic']=='Stroke'][df['Data_Value_Unit']=='per 100,000'][df['Year']=='2018'][['fips','Data_Value','LocationDesc']]
 
 import plotly.express as px
 
 fig = px.choropleth(new_df, geojson=counties, locations='fips', color='Data_Value',
-                           color_continuous_scale="Viridis",
+                           color_continuous_scale='Viridis',
                            range_color=(0, 40),
-                           scope="usa",
-                           labels={'Data_Value':'Stroke Mortality Rate Per 100,000'}
+                           scope='usa',
+                           hover_name='LocationDesc',
+                           labels=dict(Data_Value='Stroke Mortality Rate Per 100,000')
                           )
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.update_layout(margin={'r':0,'t':0,'l':0,'b':0})
 fig.show()
