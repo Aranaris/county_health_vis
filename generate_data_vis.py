2da87109a16bd1d0b2e150610db5947eb1398617
vinceye <vinceye.cm@gmail.com>
2024-01-20 16:49:38 -0800

    image file output and graph styling. separate image for each year

--- generate_data_vis.py
+++ generate_data_vis.py
@@ -10,21 +10,26 @@ def locationid_to_fips(locationid):
       return f'{locationid}'
 
 import pandas as pd
+import plotly.express as px
+
 df = pd.read_csv('data/chd_stroke_data.csv',
                  dtype={'LocationID': str})
 
 df['fips'] = df.apply(lambda x: locationid_to_fips(x['LocationID']), axis=1)
 
-new_df = df[df['Stratification1'] == 'Ages 35-64 years'][df['Topic']=='Stroke'][df['Data_Value_Unit']=='per 100,000'][df['Year']=='2018'][['fips','Data_Value','LocationDesc']]
-
-import plotly.express as px
-
-fig = px.choropleth(new_df, geojson=counties, locations='fips', color='Data_Value',
-                           color_continuous_scale='Viridis',
-                           range_color=(0, 40),
-                           scope='usa',
-                           hover_name='LocationDesc',
-                           labels=dict(Data_Value='Stroke Mortality Rate Per 100,000')
-                          )
-fig.update_layout(margin={'r':0,'t':0,'l':0,'b':0})
-fig.show()
+for year in df['Year'].unique():
+   if len(year) != 4:
+      continue
+   else:
+      new_df = df[df['Stratification1'] == 'Ages 35-64 years'][df['Topic']=='Stroke'][df['Data_Value_Unit']=='per 100,000'][df['Year']==year][['fips','Data_Value','LocationDesc']]
+      fig = px.choropleth(new_df, geojson=counties, locations='fips', color='Data_Value', width=1200, height=600,
+																color_continuous_scale='Viridis',
+																title=f"Stroke Mortality per 100,000 ({year})",
+																range_color=(0, 30),
+																scope='usa',
+																hover_name='LocationDesc',
+																labels=dict(Data_Value='Stroke Mortality Rate Per 100,000'),
+																)
+      fig.update_layout(margin={'b':0, 'l': 0}, coloraxis_colorbar=dict(title=dict(text=None), ticklabelposition='outside bottom', len=.3, thickness=15, xpad=0))
+			# fig.show()
+      fig.write_image(f"images/StrokeMortality_{year}.png")
--- get_data_source.py
+++ get_data_source.py
@@ -1,3 +1,4 @@
-import urllib.request
+from urllib.request import urlretrieve
 
-urllib.request.urlretrieve('https://data.cdc.gov/api/views/9cr5-2tt7/rows.csv?accessType=DOWNLOAD', 'data/chd_stroke_data.csv')
+urlretrieve('https://data.cdc.gov/api/views/9cr5-2tt7/rows.csv?accessType=DOWNLOAD', 'data/chd_stroke_data.csv')
+    
