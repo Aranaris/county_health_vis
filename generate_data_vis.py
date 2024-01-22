3e9a46390df530b8d1566a5301a93425d0c01613
vinceye <vinceye.cm@gmail.com>
2024-01-22 15:45:02 -0800

    using animation_frame feature for years, addressing prev commit comments

--- generate_data_vis.py
+++ generate_data_vis.py
@@ -1,35 +1,36 @@
-from urllib.request import urlopen
 import json
-with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-    counties = json.load(response)
-
-def locationid_to_fips(locationid):
-    if len(str(locationid)) < 5:
-      return f'0{locationid}'
-    else:
-      return f'{locationid}'
+from urllib.request import urlopen
 
 import pandas as pd
 import plotly.express as px
 
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+	counties = json.load(response)
+
+def locationid_to_fips(locationid):
+	if len(str(locationid)) < 5:
+		return f'0{locationid}'
+	else:
+		return f'{locationid}'
+
 df = pd.read_csv('data/chd_stroke_data.csv',
-                 dtype={'LocationID': str})
+	dtype={'LocationID': str})
 
 df['fips'] = df.apply(lambda x: locationid_to_fips(x['LocationID']), axis=1)
 
-for year in df['Year'].unique():
-   if len(year) != 4:
-      continue
-   else:
-      new_df = df[df['Stratification1'] == 'Ages 35-64 years'][df['Topic']=='Stroke'][df['Data_Value_Unit']=='per 100,000'][df['Year']==year][['fips','Data_Value','LocationDesc']]
-      fig = px.choropleth(new_df, geojson=counties, locations='fips', color='Data_Value', width=1200, height=600,
-																color_continuous_scale='Viridis',
-																title=f"Stroke Mortality per 100,000 ({year})",
-																range_color=(0, 30),
-																scope='usa',
-																hover_name='LocationDesc',
-																labels=dict(Data_Value='Stroke Mortality Rate Per 100,000'),
-																)
-      fig.update_layout(margin={'b':0, 'l': 0}, coloraxis_colorbar=dict(title=dict(text=None), ticklabelposition='outside bottom', len=.3, thickness=15, xpad=0))
-			# fig.show()
-      fig.write_image(f"images/StrokeMortality_{year}.png")
+new_df = df[df['Stratification1'] == 'Ages 35-64 years'][df['Topic']=='Stroke'][df['Data_Value_Unit']=='per 100,000']\
+	[['fips','Data_Value','LocationDesc','Year']].sort_values(by=['Year'])
+fig = px.choropleth(new_df, geojson=counties, locations='fips', 
+	color='Data_Value', 
+	width=1200, height=600,
+	color_continuous_scale='Viridis',
+	title='Stroke Mortality per 100,000',
+	animation_frame='Year',
+	range_color=(0, 40),
+	scope='usa',
+	hover_name='LocationDesc',
+	labels={'Data_Value':'Stroke Mortality Rate Per 100,000'},
+	)
+fig.update_layout(margin={'b':0, 'l': 0}, coloraxis_colorbar={'title':{'text':None}, 
+	'ticklabelposition':'outside bottom', 'len':.5, 'thickness':15, 'xpad':0})
+fig.show()
--- get_data_source.py
+++ get_data_source.py
@@ -1,4 +1,6 @@
-from urllib.request import urlretrieve
+from urllib.request import urlopen
 
-urlretrieve('https://data.cdc.gov/api/views/9cr5-2tt7/rows.csv?accessType=DOWNLOAD', 'data/chd_stroke_data.csv')
-    
+with urlopen('https://data.cdc.gov/api/views/9cr5-2tt7/rows.csv?accessType=DOWNLOAD') as response, \
+open('data/chd_stroke_data.csv', 'wb') as out_file:
+	data = response.read()
+	out_file.write(data)
