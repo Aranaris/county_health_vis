beb6d1da8270761e4e97568ba36db15a1b3de89b
vinceye <vinceye.cm@gmail.com>
2024-01-23 17:20:59 -0800

    refactoring get_data_source to use shutil

--- get_data_source.py
+++ get_data_source.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen
+import shutil
 
 with urlopen('https://data.cdc.gov/api/views/9cr5-2tt7/rows.csv?accessType=DOWNLOAD') as response, \
 open('data/chd_stroke_data.csv', 'wb') as out_file:
-	data = response.read()
-	out_file.write(data)
+	shutil.copyfileobj(response, out_file)
