82728837465ae182693244c4dd3aae6a0e77dd2d
vinceye <vinceye.cm@gmail.com>
2024-01-23 17:24:53 -0800

    adding comments to outline each data step

--- generate_data_vis.py
+++ generate_data_vis.py
@@ -4,9 +4,11 @@ from urllib.request import urlopen
 import pandas as pd
 import plotly.express as px
 
+# get GeoJSON data for county map
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
 	counties = json.load(response)
 
+# reading source csv and updating locationid to be compatible with fips
 def locationid_to_fips(locationid):
 	if len(str(locationid)) < 5:
 		return f'0{locationid}'
@@ -18,10 +20,15 @@ df = pd.read_csv('data/chd_stroke_data.csv',
 
 df['fips'] = df.apply(lambda x: locationid_to_fips(x['LocationID']), axis=1)
 
+# filtering down dataframe for generating a meaningful graph for each year
+# key parameters are age group stratification, stroke vs chd, and per 100,000 unit
+
 new_df = df.loc[df['Stratification1'] == 'Ages 35-64 years']\
 	.loc[df['Topic']=='Stroke']\
 	.loc[df['Data_Value_Unit']=='per 100,000']\
 	[['fips','Data_Value','LocationDesc','Year']].sort_values(by=['Year'])
+
+# generating figure with set parameters
 
 fig = px.choropleth(new_df, geojson=counties, locations='fips', 
 	color='Data_Value', 
@@ -37,3 +44,4 @@ fig = px.choropleth(new_df, geojson=counties, locations='fips',
 fig.update_layout(margin={'b':0, 'l': 0}, coloraxis_colorbar={'title':{'text':None}, 
 	'ticklabelposition':'outside bottom', 'len':.5, 'thickness':15, 'xpad':0})
 fig.show()
+
